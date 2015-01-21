0ea88dfd9fffa6786a180d46321ba115bc2ac164
Connor Denman <connor.denman@gmail.com>
2015-01-21 12:49:30 -0700

    Initial Commit

---
+++ Huangify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Huangify
+//
+//  Created by Connor Denman on 1/21/15.
+//  Copyright (c) 2015 Culture Engineers LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
